605b7c1133c639b0e91ae755db4c97137a61ea25
Carlos Silva <silvaugusto222@gmail.com>
2023-03-12 16:21:25 -0300

    alterações de pastas

--- portifolio/src/App.jsx
+++ portifolio/src/App.jsx
@@ -1,32 +1,43 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState([])
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/carloosf")
+      .then((res) => res.json())
+      .then((json) => setUser(json))
+  }, [])
+
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+        <input type="text"
+            placeholder='Pesquisar Usuario'
+            //onClick={}
+            />
+
+      {user && (
+        <div className='container'>
+
+        
+
+          <a href={user.html_url} target='_blank' > <img src={user.avatar_url} /></a>
+          <h2>{user.name}</h2>
+
+          <div className='infos'>
+            <p>{user.blog}</p>
+            <p>{user.created_at}</p>
+            <p>{user.name}</p>
+            <p>{user.login}</p>
+            <p>{user.location}</p>
+          </div>
+        </div>
+      )
+      }
     </div>
   )
 }
