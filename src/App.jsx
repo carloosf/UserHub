eb4a7a473a5cc4feb35a6453334002216c2b419b
Carlos Silva <88803987+carloosf@users.noreply.github.com>
2023-03-13 14:20:08 -0300

    Search Funcional

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,49 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [user, setUser] = useState([])
+  const [user, setUser] = useState(null)
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
-    fetch("https://api.github.com/users/carloosf")
-      .then((res) => res.json())
-      .then((json) => setUser(json))
-  }, [])
+    const fetchUser = async () => {
+      const response = await fetch(`https://api.github.com/users/${search}`)
+      const data = await response.json()
+      setUser(data)
+    }
 
+    if (search) {
+      fetchUser()
+    }
+  }, [search])
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  };
 
   return (
     <div className="App">
-      
+      <div>
         <input type="text"
-            placeholder='Pesquisar Usuario'
-            //onClick={}
-            />
+          placeholder='Pesquisar Usuario'
+          value={search}
+          onChange={handleChange}
+        />
+      </div>
+
 
       {user && (
         <div className='container'>
 
-        
+
 
           <a href={user.html_url} target='_blank' > <img src={user.avatar_url} /></a>
-          <h2>{user.name}</h2>
+          <h2>{user.login}</h2>
+
 
           <div className='infos'>
+            <p>{user.name}</p>
             <p>{user.blog}</p>
             <p>{user.created_at}</p>
-            <p>{user.name}</p>
-            <p>{user.login}</p>
             <p>{user.location}</p>
           </div>
         </div>
