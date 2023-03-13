774b7c7e4c411016fd9f80a40ae60df3d98671a3
Carlos Silva <88803987+carloosf@users.noreply.github.com>
2023-03-13 16:10:13 -0300

    CSS Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
@@ -23,34 +22,34 @@ function App() {
   };
 
   return (
+
     <div className="App">
-      <div>
-        <input type="text"
-          placeholder='Pesquisar Usuario'
-          value={search}
-          onChange={handleChange}
-        />
-      </div>
+      <div className='main'>
+        <form action="">
+          <input type="text"
+            placeholder='Pesquisar Usuario'
+            onChange={handleChange}
+            value={search}
+          />
+          <button type="submit"></button>
+        </form>
 
+        {user && (
+          <div className='container'>
 
-      {user && (
-        <div className='container'>
+            <a href={user.html_url} target='_blank' > <img src={user.avatar_url} /></a>
+            <h2>{user.login}</h2>
 
-
-
-          <a href={user.html_url} target='_blank' > <img src={user.avatar_url} /></a>
-          <h2>{user.login}</h2>
-
-
-          <div className='infos'>
-            <p>{user.name}</p>
-            <p>{user.blog}</p>
-            <p>{user.created_at}</p>
-            <p>{user.location}</p>
+            <div className='infos'>
+              <p>{user.name}</p>
+              <p>{user.blog}</p>
+              <p>{user.created_at}</p>
+              <p>{user.location}</p>
+            </div>
           </div>
-        </div>
-      )
-      }
+        )
+        }
+      </div>
     </div>
   )
 }
